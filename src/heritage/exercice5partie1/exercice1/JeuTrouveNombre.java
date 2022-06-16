2c9dc778bd178bb467b72fdf376ad2de76e6f6ed
Mysteriou85 <guib5@hotmail.fr>
2022-06-16 16:39:15 +0200

    Exercice 5 partie 1 exercie 1

--- src/heritage/exercice5partie1/exercice1/JeuTrouveNombre.java
+++ src/heritage/exercice5partie1/exercice1/JeuTrouveNombre.java
@@ -7,7 +7,7 @@ public class JeuTrouveNombre {
     private Scanner scanner = new Scanner(System.in);
     private Random random = new Random();
     private int nombreAlea = random.nextInt(100) + 1;
-    private int tentative;
+    private int tentative = 10;
 
     public int getNombreAlea() {
         return nombreAlea;
@@ -15,5 +15,43 @@ public class JeuTrouveNombre {
 
     public int getTentative() {
         return tentative;
+    }
+
+    public void jeu() {
+        System.out.println("Veuillez entrer un nombre entre 1 et 100");
+        System.out.printf("Vous avez %d tentative \n", tentative);
+        while (tentative != 0) {
+            int result = scanner.nextInt();
+            if(result < nombreAlea) {
+                System.out.println("Trop petit !");
+                tentative--;
+                System.out.println("Essais restant : " + tentative);
+            } else if(result > nombreAlea) {
+                System.out.println("Trop grand !");
+                tentative--;
+                System.out.println("Essais restant : " + tentative);
+            } else if(result == nombreAlea) {
+                System.out.println("Gagnez");
+                restart();
+            }
+            if(tentative == 0) {
+                System.out.println("Vous avez perdu");
+                restart();
+            }
+        }
+    }
+
+    public void restart () {
+        System.out.println("Voulez-vous refaire une partie ?");
+        System.out.println("1. oui");
+        System.out.println("2. Non");
+        int restart = scanner.nextInt();
+        if(restart == 1) {
+            tentative = 10;
+        } else if (restart == 2) {
+            System.exit(0);
+        } else {
+            System.out.println("Mauvaise entrée");
+        }
     }
 }
--- src/heritage/exercice5partie1/exercice1/TestJeuTrouveNombre.java
+++ src/heritage/exercice5partie1/exercice1/TestJeuTrouveNombre.java
@@ -2,6 +2,7 @@ package heritage.exercice5partie1.exercice1;
 
 public class TestJeuTrouveNombre {
     public static void main(String[] args) {
-
+        JeuTrouveNombre jeuNombre = new JeuTrouveNombre();
+        jeuNombre.jeu();
     }
 }
